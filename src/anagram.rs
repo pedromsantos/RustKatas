07bec97aef1ab199618fe1f62221a3191ed62d2f
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-08-27 19:06:51 +0200

    refactor: convert to for_each

--- src/anagram.rs
+++ src/anagram.rs
@@ -4,21 +4,19 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet
     let mut word_chars: Vec<char> = word.to_lowercase().chars().collect();
     let mut anagrams: HashSet<&'a str> = HashSet::new();
 
-    for possible_anagram in possible_anagrams.iter() {
+    possible_anagrams.iter().for_each(|possible_anagram| {
         let mut possible_anagram_chars: Vec<char> =
             possible_anagram.to_lowercase().chars().collect();
 
-        if possible_anagram_chars == word_chars {
-            continue;
-        }
+        if possible_anagram_chars != word_chars {
+            possible_anagram_chars.sort();
+            word_chars.sort();
 
-        possible_anagram_chars.sort();
-        word_chars.sort();
-
-        if word_chars == possible_anagram_chars {
-            anagrams.insert(possible_anagram);
+            if word_chars == possible_anagram_chars {
+                anagrams.insert(possible_anagram);
+            }
         }
-    }
+    });
 
     anagrams
 }
