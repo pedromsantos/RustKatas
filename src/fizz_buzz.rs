f6a7a54eeef98d729e4389a93e63cf9ce3189652
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-08-30 11:41:25 +0200

    refactor: reorder match

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -10,8 +10,8 @@ pub fn fizz_buzzer(number: u8) -> String {
 pub fn raindrops(n: u32) -> String {
     match (n % 3, n % 5, n % 7) {
         (0, 0, 0) => "PlingPlangPlong".to_string(),
-        (0, _, 0) => "PlingPlong".to_string(),
         (0, 0, _) => "PlingPlang".to_string(),
+        (0, _, 0) => "PlingPlong".to_string(),
         (_, 0, 0) => "PlangPlong".to_string(),
         (0, _, _) => "Pling".to_string(),
         (_, 0, _) => "Plang".to_string(),
